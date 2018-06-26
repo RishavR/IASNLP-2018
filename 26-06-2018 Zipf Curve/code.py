0b32cdbf464e71e8f7064d7e9f9700ac009175a8
rishav-r <rishav.main@gmail.com>
2018-06-26 22:54:43 +0530

    Add files via upload

--- 26-06-2018 Zipf Curve/code.py
+++ 26-06-2018 Zipf Curve/code.py
@@ -8,35 +8,46 @@ Created on Tue Jun 26 20:48:26 2018
 import nltk as nlp
 import re
 import matplotlib.pyplot as plt 
-
-paragraph= """ The United States which has long sought Japanese action to stimulate its economy appears to be satisfied Tokyos latest package is a major development and allows leading industrial nations to reaffirm their agreement to stabilize currencies
-    
-Monetary sources said they believed that US Treasury Secretary James Baker considered Tokyos package announced yesterday to be a major stimulation of the Japanese economy
-    
-But yesterdays statement by seven leading industrial powers endorses the yens rise from around 153 to the dollar the level at the February 22 Paris Accord to about 145 today
-    
-The supplementary budget worth about 34 billion dlrs was announced by the ruling Liberal Democratic Party on the eve of Miyazawas departure for Washington to attend yesterdays meetings of leading industrial nations
-    
-In a strongly worded statement terming the Japanese action extraordinary and urgent the meeting reaffirmed the Paris Accord by noting that current exchange rates are within ranges broadly consistent with fundamentals or economic reality
-    
-The Group of Seven the United States Japan West Germany France Britain Italy and Canada therefore repeated their willingness to continue close cooperation to foster exchange rate stability
-    
-The cooperation agreement has resulted in concerted central bank intervention of 8 billion to 9 billion dlrs to halt the dollars fall"""
-
-paragraph= re.sub(r'[^\w]', ' ', paragraph)
-tokens=nlp.word_tokenize(paragraph)
+#open the plaintext file  
+file=open("The_Gift_of_The_Magi.txt")
+# Read the file into a string type variable called 'text'
+text=file.read()
+#Using Regex (Regular Expressions) remove all the special characters in the text.
+#This is done by selecting all special characters and substituting them with a space character.  
+text= re.sub(r'[^\w]', ' ', text)
+# Tokenize the text and store it in a list called tokens 
+tokens=nlp.word_tokenize(text)
+# Using the Counter function, obtatin the frequency of each token and store it as a 'key - value' 
+# Pair in a dictionary called types 
 types=nlp.Counter(tokens)
+# For Debugging purposes 
 print(tokens)
 print(types)
+# Extract all the 'keys' from the dictionary types and store it as a list in X
 X=list(types.keys())
+# Declare an empty list Y
 Y=[]
+#For Debugging purposes
 print(X)
+# For each item of key in X, find the corresponding value in the dictionary types and store it in list Y
 for key in X: 
     Y.append(types[key])
+# For Debugging purposes
 print(Y)
-plt.figure(figsize=(50,12))
+
+############ PLOTTING THE GRAPH ##############
+
+#Here we change the figure size to accomodate our huge X axis of Tokens
+plt.figure(figsize=(200,12))
+# Perform scatter plotting
 plt.scatter(X,Y)
+# Label on X axis named as Tokens
 plt.xlabel("TOKENS")
+# Label on Y axis named as Frequency
 plt.ylabel("FREQUENCY")
+# Changing the orientation of X axis labels (Ticks from now on) to vertical orientation
 plt.xticks(rotation='vertical')
+#Saving the figure in the PWD (present working directory)
 plt.savefig("ZIFS_plot.png")
+
+########### THE END ############# 
